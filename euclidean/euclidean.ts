4fa2594159833600aa8b35a0e8aefa8f128d5207
Luis Cazuriaga <luis.cazuriaga@gmail.com>
2021-01-13 19:20:33 -0300

    feat: refactoring euclidean with mmc

--- euclidean/euclidean.ts
+++ euclidean/euclidean.ts
@@ -1,12 +1,12 @@
 const euclideanAlgorithm = (N, M) => {
-    let array = [0]
-    let oldNumber = 0
-    let counter = 0
-    for (let i in Array(N - 1).fill(0)) {
-        array.push((oldNumber + M) % N)
-        oldNumber = array[i]
+    let a = N
+    let b = M
+    while (b != 0) {
+        let resto = b
+        b = a % b
+        a = resto
     }
-    return array.filter((elem, index, arr) => arr.indexOf(elem) === index).length
+    return N / a
 }
 
 console.log(euclideanAlgorithm(10, 4))
